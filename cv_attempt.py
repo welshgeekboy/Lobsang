01891f4a0fc7a3b7889ffaf0b1794d6efa85ce97
Finley Watson <few720@gmail.com>
2016-08-03 14:51:17 +0000

    Start work on OpenCV programs.

--- cv_attempt.py
+++ cv_attempt.py
@@ -1,12 +1,29 @@
 import Lobsang
+import cv2.cv as cv
 import cv2
-import numpy
 import picamera
 import picamera.array
+import time
+import numpy
 
 Lobsang.begin()
 
-with picamera.PiCamera() as cam:
-	with picamera.array
-		while True:
-		
+camera = picamera.PiCamera()
+camera.resolution = (256, 256)
+cv.NamedWindow("Image", 1)
+
+try:
+	while True:
+		with picamera.array.PiRGBArray(camera) as stream:
+			camera.capture(stream, format="bgr", resize=(256, 256))
+			#image = numpy.frombuffer(stream.array, dtype=numpy.uint8)
+			image = cv2.imdecode(stream.array, 1)
+			print image
+			print "------------------------"
+			cv.ShowImage("Image", image)
+			stream.truncate(0)
+except Exception as error:
+	print error
+	cv.DestroyAllWindows()
+	Lobsang.quit()
+	camera.close()
